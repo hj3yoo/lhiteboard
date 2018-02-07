54faac37b0c9292ee6fa1ab270d4c0187dd15311
Edmond Yoo <Edmond.Yoo@ibm.com>
2018-02-07 17:48:06 -0500

    Optimizing, debugging

--- src/detect.py
+++ src/detect.py
@@ -3,6 +3,7 @@ import copy
 from matplotlib import pyplot as plt
 import math
 import numpy as np
+from datetime import datetime
 
 # Tuple of RGB values for special colours
 COLOR_WHITE = (255, 255, 255)
@@ -12,7 +13,7 @@ COLOR_GREEN = (0, 255, 0)
 COLOR_RED = (0, 0, 255)
 
 
-def find_coordinate(img, blur_size=5, threshold=50, percentile=10, pyramid_height=7, centre_ratio=0.25):
+def find_coordinate(img, blur_size=5, threshold=50, percentile=10, pyramid_height=7, centre_ratio=0.25, debug=False):
     """
     Core CV algorithm to locate the intended coordinate of the point given the reflections on the surface
     :param img: source image to run the algorithm on
@@ -24,6 +25,8 @@ def find_coordinate(img, blur_size=5, threshold=50, percentile=10, pyramid_heigh
     :return: x, y coordinate of the point
     """
     # Convert image to grayscale, apply blurring and thresholding
+    if debug:
+        t_start = datetime.now()
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_blur = cv2.GaussianBlur(img_gray, (blur_size, blur_size), 0)
     img_thresh = cv2.threshold(img_blur, threshold, 255, cv2.THRESH_TOZERO)[1]
@@ -31,7 +34,7 @@ def find_coordinate(img, blur_size=5, threshold=50, percentile=10, pyramid_heigh
     ellipses = [cv2.fitEllipse(cont) for cont in contours if len(cont) >= 5]
     if len(ellipses) == 0:
         #print('No ellipse found')
-        return -1, -1
+        return (-1, -1)
     # Sorted by the size of the ellipses in descending order
     def get_key(item):
         return item[1][0] * item[1][1]
@@ -47,19 +50,26 @@ def find_coordinate(img, blur_size=5, threshold=50, percentile=10, pyramid_heigh
     new_pt_y = int((ellipse[0][1] + max_loc[1]) / 2 + 0.5)
     new_pt = (new_pt_x, new_pt_y)
 
-    # Mark the locations in the debug image
-    #ellipse_centre = (int(ellipse[0][0] + 0.5), int(ellipse[0][1] + 0.5))
-    #img_debug_show = cv2.cvtColor(img_thresh, cv2.COLOR_GRAY2BGR)
-    #img_debug_show = cv2.circle(img_debug_show, max_loc, 1, COLOR_RED, 1)
-    #img_debug_show = cv2.ellipse(img_debug_show, ellipse, COLOR_GREEN, 1)
-    #img_debug_show = cv2.circle(img_debug_show, ellipse_centre, 1, COLOR_RED, 1)
-    #img_debug_show = cv2.circle(img_debug_show, new_pt, 1, COLOR_BLUE, 1)
-    #cv2.imshow('coordinates', img_debug_show)
-    #cv2.waitKey(0)
-    return new_pt
+    if debug:
+        t_end = datetime.now()
+        t_delta = t_end - t_start
+        print("Coordinate detection: %.5f seconds" % t_delta.total_seconds())
+        # Mark the locations in the debug image
+        ellipse_centre = (int(ellipse[0][0] + 0.5), int(ellipse[0][1] + 0.5))
+        img_debug_show = cv2.cvtColor(img_thresh, cv2.COLOR_GRAY2BGR)
+        img_debug_show = cv2.circle(img_debug_show, max_loc, 1, COLOR_RED, 1)
+        img_debug_show = cv2.ellipse(img_debug_show, ellipse, COLOR_GREEN, 1)
+        img_debug_show = cv2.circle(img_debug_show, ellipse_centre, 1, COLOR_RED, 1)
+        img_debug_show = cv2.circle(img_debug_show, new_pt, 1, COLOR_BLUE, 1)
+        cv2.imshow('coordinates', img_debug_show)
+        cv2.waitKey(0)
+    if debug:
+        return new_pt, t_delta.total_seconds()
+    else:
+        return new_pt
 
 
-def find_source(img, blur_size=5, threshold=40, neighbour_ratio=0.5):
+def find_source(img, blur_size=5, threshold=40, neighbour_ratio=0.5, debug=False):
     """
     Extract the important sections of images where light sources are found
     :param img: source image
@@ -68,6 +78,8 @@ def find_source(img, blur_size=5, threshold=40, neighbour_ratio=0.5):
     :param neighbour_ratio: percentage of spaces near the detected area to be considered
     :return: x, y, width, height of the source
     """
+    if debug:
+        t_start = datetime.now()
     ret = []
     # Filtering - convert to grayscale, apply Gaussian blur, then remove noises with threshold
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -83,11 +95,17 @@ def find_source(img, blur_size=5, threshold=40, neighbour_ratio=0.5):
         y0 = int(y - h * neighbour_ratio / 2)
         y1 = int(y + h * (1 + neighbour_ratio / 2))
         ret.append(((x0, y0), (x1, y1)))
-        # cv2.imshow('source', img[y0:y1, x0:x1])
-        # cv2.waitKey(0)
+        if debug:
+            cv2.imshow('source', img[y0:y1, x0:x1])
+            cv2.waitKey(0)
 
-    #img_debug_show = cv2.resize(img_thresh, (len(img_thresh[0]) // 4, len(img_thresh) // 4))
-    #cv2.imshow('threshold', img_debug_show)
+    if debug:
+        t_end = datetime.now()
+        t_delta = t_end - t_start
+        print("Source detection: %.5f seconds" % t_delta.total_seconds())
+
+        img_debug_show = cv2.resize(img_thresh, (len(img_thresh[0]) // 4, len(img_thresh) // 4))
+        cv2.imshow('threshold', img_debug_show)
     '''
     # NOTE: this snippet will omit all sources but the largest one
     # TODO: Convert to multi-coordinate detection
@@ -97,6 +115,9 @@ def find_source(img, blur_size=5, threshold=40, neighbour_ratio=0.5):
     ret = sorted(ret, key=get_key, reverse=True)
     return [ret[0]]
     '''
-    return ret
+    if debug:
+        return ret, t_delta.total_seconds()
+    else:
+        return ret
     pass
 
--- src/test/test_detect.py
+++ src/test/test_detect.py
@@ -4,6 +4,7 @@ import os
 import sys
 import math
 
+
 def detection_test(arg_dict):
     """
     Test the point detection algorithm using a given test data
@@ -11,41 +12,56 @@ def detection_test(arg_dict):
     """
     img_dir = os.path.abspath(arg_dict['img_dir'])
     img_list = [x for x in os.listdir(img_dir) if x.endswith('.jpg') or x.endswith('.png')]
-    print('Number of test images: %d' % len(img_list))
+    if arg_dict['debug']:
+        print('Number of test images: %d' % len(img_list))
     """
     Coordinate detection can be computationally heavy for the entire image
     Therefore, image is cropped into a smaller sections where the IR reflection is detected
     For each cropped section, coordinate detection will be run
     """
     for img_name in img_list:
-        print(img_name + ':')
+        if arg_dict['debug']:
+            print(img_name + ':')
+            img_show = copy.copy(img)
+            new_x0 = math.inf
+            new_y0 = math.inf
+            new_x1 = -1
+            new_y1 = -1
+            t_start = datetime.now()
         img = cv2.imread(os.path.join(img_dir, img_name))
-        img_show = copy.copy(img)
-        sources = find_source(img)
-        new_x0 = math.inf
-        new_y0 = math.inf
-        new_x1 = -1
-        new_y1 = -1
+        if arg_dict['debug']:
+            sources, t_source = find_source(img)
+        else:
+            sources = find_source(img)
         # Each sections are represented by a rectangle
-        for pt0, pt1 in sources:
+        for pt0, pt1 in sources[:3]:
             x0, y0 = pt0
             x1, y1 = pt1
-            det_pt = find_coordinate(img[y0:y1, x0:x1])
-            if det_pt[0] != -1 and det_pt[1] != -1:
-                # Mark the detected point for each section
-                img_show = cv2.circle(img_show, (x0 + det_pt[0], y0 + det_pt[1]), arg_dict['pt_radius'], COLOR_RED, 3)
-            img_show = cv2.rectangle(img_show, pt0, pt1, COLOR_GREEN, 3)
-            new_x0 = min(new_x0, x0)
-            new_y0 = min(new_y0, y0)
-            new_x1 = max(new_x1, x1)
-            new_y1 = max(new_y1, y1)
+            if arg_dict['debug']:
+                det_pt, t_coord = find_coordinate(img[y0:y1, x0:x1])
+            else:
+                det_pt = find_coordinate(img[y0:y1, x0:x1])
+            if arg_dict['debug']:
+                if det_pt[0] != -1 and det_pt[1] != -1:
+                    # Mark the detected point for each section
+                    img_show = cv2.circle(img_show, (x0 + det_pt[0], y0 + det_pt[1]), arg_dict['pt_radius'], COLOR_RED, 3)
+                img_show = cv2.rectangle(img_show, pt0, pt1, COLOR_GREEN, 3)
+                new_x0 = min(new_x0, x0)
+                new_y0 = min(new_y0, y0)
+                new_x1 = max(new_x1, x1)
+                new_y1 = max(new_y1, y1)
 
-        # Display the result to the user, and pause until user proceeds
-        #cv2.imshow('Result zoomed', img_show[new_y0-10:new_y1+10, new_x0-10:new_x1+10])
-        img_show = cv2.resize(img_show, (len(img[0]) // 4, len(img) // 4))
-        cv2.imshow('Result', img_show)
-        if not arg_dict['animate']:
-            cv2.waitKey(0)
+        if arg_dict['debug']:
+            t_end = datetime.now()
+            t_delta = t_end - t_start
+            print("%.5f, %.5f, %.5f" % (t_delta.total_seconds(), t_source, t_coord))
+
+            # Display the result to the user, and pause until user proceeds
+            cv2.imshow('Result zoomed', img_show[new_y0-10:new_y1+10, new_x0-10:new_x1+10])
+            img_show = cv2.resize(img_show, (len(img[0]) // 4, len(img) // 4))
+            cv2.imshow('Result', img_show)
+            if not arg_dict['animate']:
+                cv2.waitKey(0)
     return
 
 
@@ -56,6 +72,7 @@ if __name__ == '__main__':
                         help='Quickly animate through all test images instead of manual inspection')
     parser.add_argument('-ptr', dest='pt_radius', type=int, default=10,
                         help='Radius of the circles to mark input points')
+    parser.add_argument('-deb', dest='debug', action='store_true', help='Enable debugging features')
     args = parser.parse_args()
     dict_args = vars(args)
     in_dir = os.path.abspath(dict_args['img_dir'])
